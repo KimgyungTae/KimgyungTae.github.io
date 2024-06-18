9cd11736ebbcf8f635f4816536dc8e51207ecc70
김경태(한남대) <146739036+KimgyungTae@users.noreply.github.com>
2024-06-18 17:43:16 +0900

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,10 @@
+function check(form) 
+{
+    if(form.userid.value=="kenny2019" && form.userpassword.value=="eag5886899678")
+        {
+            window.open("login.html")
+        }
+        else{
+            alert("Error ID or Password");
+        }
+}
